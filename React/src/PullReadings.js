29958a8e74284cdbadc6a7a063fa9880c2638472
Alec Hajinoff <hajinoff@gmail.com>
2025-10-17 06:42:10 +0100

    17.10.25, 6.42

--- React/src/PullReadings.js
+++ React/src/PullReadings.js
@@ -98,7 +98,8 @@ function PullReadings() {
               {historyError}
             </div>
           )}
-          {/* Render a table when we have historic rows, otherwise show a friendly notice. */}
+          // Render a table when we have historic rows, otherwise show a
+          friendly notice.
           {historyData.length > 0 ? (
             <div className="table-responsive">
               <table className="table table-striped">
