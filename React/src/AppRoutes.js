e52350a9b4e9fa4433d8dada695a1666ace832b7
Alec Hajinoff <hajinoff@gmail.com>
2025-10-20 17:49:38 +0100

    20.10.25, 17.49

--- React/src/AppRoutes.js
+++ React/src/AppRoutes.js
@@ -15,35 +15,3 @@ export default function AppRoutes() {
     </Routes>
   );
 }
-
-/*
-//import logo from './logo.svg';
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./Header";
-import MainRegLog from "./MainRegLog";
-import Footer from "./Footer";
-import RegisteredPage from "./RegisteredPage";
-import PullReadings from "./PullReadings";
-import LogoutComponent from "./LogoutComponent";
-
-function App() {
-  return (
-    <div>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<MainRegLog />} />
-          <Route path="RegisteredPage" element={<RegisteredPage />} />
-          <Route path="PullReadings" element={<PullReadings />} />
-          <Route path="LogoutComponent" element={<LogoutComponent />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
-  );
-}
-
-export default App;
-*/
